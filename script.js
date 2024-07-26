c66e128270c7a2cd2d9892b276cab5a1131c13e5
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-07-26 05:46:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+/* This is where you'll complete the Milestone. Open your README.md file and click 'Open Preview' for detailed instuctions! */
+
+let dramaShows = [
+  "Ginny & Georgia",
+  "Outlander",
+  "Grey's Anatomy",
+  "Ozark",
+  "The Queen's Gambit"
+];
+
+let fantasyShows = [
+  "Supernatural",
+  "The Sandman",
+  "Wednesday",
+  "The Witcher",
+  "Avatar: The Last Airbender"
+];
+
+let comedyShows = [
+  "Arrested Development",
+  "Dead to Me",
+  "Seinfeld",
+  "Emily in Paris",
+  "The Good Place"
+];
+
+function chooseRandomGenre() {
+ // Deliverable 1: Your code here 👇
+  let num = getRandomNumber(0, 2);
+  if (num === 0){
+    return "Drama"
+  } else if (num === 1){
+      return "Fantasy"
+    } else {
+        return "Comedy"
+    };
+ 
+  
+};
+
+function displayRandomShow(genre) {
+  // Deliverable 2: Your code here 👇
+  if ( genre === "random"){
+    genre=chooseRandomGenre();
+  }
+  // our arrays contain 5 items so that is why i used min to be 0 and max to be 4. This provids a random index everytime it runs ensuring randomization
+  let Index = getRandomNumber(0,4);
+
+  // display a show
+  if (genre === "drama"){
+    displayShow(dramaShows[Index]);
+  }else if (genre === "fantasy") {
+    displayShow(fantasyShows[Index]);
+  }
+  else {
+    displayShow(comedyShows[Index]);
+  }
+}
+
